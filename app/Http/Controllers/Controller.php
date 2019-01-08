316750b87d9f4b18008e2a5e306daa86d0204c07
kang <1501325740@qq.com>
2019-01-08 18:35:51 +0800

    添加了一个Controler 的  abc方法

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	public function abc(){
+	echo 1;
+	}
 }
